a72df28c0177d4d4c93ef005c5b8722b3448d67c
양효빈 <hyobin3026@naver.com>
2025-01-03 14:38:13 +0900

    feat: main 컨트롤러 구현

---
+++ src/main/java/springSecurity/practiceSecurity/controller/MainController.java
@@ -0,0 +1,20 @@
+package springSecurity.practiceSecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+
+    @GetMapping("/user")
+    public String userPage(){
+        return "user page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage(){
+        return "admin page";
+    }
+}
