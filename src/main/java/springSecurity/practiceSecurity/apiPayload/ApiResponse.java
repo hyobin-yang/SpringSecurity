23609f353d87f64bc9eaf8a1183562fd629f0fca
양효빈 <hyobin3026@naver.com>
2025-01-07 16:06:19 +0900

    feat(ApiResponse): api 응답 내용 정의

---
+++ src/main/java/springSecurity/practiceSecurity/apiPayload/ApiResponse.java
@@ -0,0 +1,33 @@
+package springSecurity.practiceSecurity.apiPayload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result; // 어떤 형태의 값이 올지 모르므로 제네릭으로 열기
+
+    // 성공 시 응답 생성
+    //public static <T> ApiResponse<T> onSuccess(T result){
+        //return new ApiResponse<>(true, SuccessStatus._OK.getCode(), SuccessStatus._OK.getMessage(), result);
+    //}
+
+    //public static <T> ApiResponse<T> of(BaseCode code, T result){
+        //        return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
+//    }
+
+    // 실패 시 응답 생성
+    public static <T> ApiResponse<T> onFailure(String code, String message, T data){
+        return new ApiResponse<>(false, code, message, data);
+    }
+
+}
