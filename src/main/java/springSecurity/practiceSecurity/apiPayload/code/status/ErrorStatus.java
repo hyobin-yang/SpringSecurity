d22658ea9e06f65e03ae857d6be5d61aa41ef333
양효빈 <hyobin3026@naver.com>
2025-01-07 17:54:49 +0900

    refactor(ErrorStatus): 에러 코드 추가

--- src/main/java/springSecurity/practiceSecurity/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/springSecurity/practiceSecurity/apiPayload/code/status/ErrorStatus.java
@@ -10,13 +10,26 @@ import springSecurity.practiceSecurity.apiPayload.code.dto.ErrorReasonDTO;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
 
-    _BAD_REQUEST(HttpStatus.BAD_REQUEST, "ERROR400", "400 에러"),
-    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "ERROR401", "401 에러"),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "ERROR403", "403 에러"),
-    _NOT_FOUND(HttpStatus.NOT_FOUND, "ERROR404", "404 에러"),
+    // 서버 에러
+    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
+    _GATEWAY_TIMEOUT(HttpStatus.GATEWAY_TIMEOUT, "COMMON504", "서버 에러, 관리자에게 문의 바랍니다."),
 
-    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "ERROR500", "500 에러"),
-    _GATEWAY_TIMEOUT(HttpStatus.GATEWAY_TIMEOUT, "ERROR504", "504 에러");
+    // 일반 에러
+    _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+    _NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404", "찾을 수 없는 요청입니다."),
+
+    // 멤버 관련 에러
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "존재하지 않는 사용자입니다."),
+    EMAIL_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "이메일은 필수입니다."),
+    EMAIL_INVALID(HttpStatus.BAD_REQUEST, "MEMBER4003", "잘못된 이메일 형식입니다."),
+    PASSWORD_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4004", "비밀번호는 필수입니다."),
+    PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "MEMBER4005", "잘못된 비밀번호입니다."),
+
+    // 리뷰 관련 에러
+    REVIEW_CONTENT_LENGTH_INVALID(HttpStatus.BAD_REQUEST, "REVIEW4001", "리뷰 내용은 10자 이상, 100자 이하여야 합니다."),
+    REVIEW_IMAGE_INVALID(HttpStatus.BAD_REQUEST, "REVIEW4002", "잘못된 이미지 형식입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
