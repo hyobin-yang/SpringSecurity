91bedcc7905654fc3f81aaaa7a4895dfdff77f61
양효빈 <hyobin3026@naver.com>
2025-01-10 22:17:30 +0900

    refactor: 에러코드, 성공코드 추가

--- src/main/java/springSecurity/practiceSecurity/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/springSecurity/practiceSecurity/apiPayload/code/status/ErrorStatus.java
@@ -20,16 +20,13 @@ public enum ErrorStatus implements BaseErrorCode {
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
     _NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404", "찾을 수 없는 요청입니다."),
 
-    // 멤버 관련 에러
-    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "존재하지 않는 사용자입니다."),
-    EMAIL_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "이메일은 필수입니다."),
-    EMAIL_INVALID(HttpStatus.BAD_REQUEST, "MEMBER4003", "잘못된 이메일 형식입니다."),
-    PASSWORD_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4004", "비밀번호는 필수입니다."),
-    PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "MEMBER4005", "잘못된 비밀번호입니다."),
+    // 유저 관련 에러
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4001", "존재하지 않는 사용자입니다."),
+    EMAIL_NOT_EXIST(HttpStatus.BAD_REQUEST, "USER4002", "이메일은 필수입니다."),
+    EMAIL_INVALID(HttpStatus.BAD_REQUEST, "USER4003", "잘못된 이메일 형식입니다."),
+    PASSWORD_NOT_EXIST(HttpStatus.BAD_REQUEST, "USER4004", "비밀번호는 필수입니다."),
+    PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "USER4005", "잘못된 비밀번호입니다.");
 
-    // 리뷰 관련 에러
-    REVIEW_CONTENT_LENGTH_INVALID(HttpStatus.BAD_REQUEST, "REVIEW4001", "리뷰 내용은 10자 이상, 100자 이하여야 합니다."),
-    REVIEW_IMAGE_INVALID(HttpStatus.BAD_REQUEST, "REVIEW4002", "잘못된 이미지 형식입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/springSecurity/practiceSecurity/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/springSecurity/practiceSecurity/apiPayload/code/status/SuccessStatus.java
@@ -10,8 +10,37 @@ import springSecurity.practiceSecurity.apiPayload.code.dto.ReasonDTO;
 @AllArgsConstructor
 public enum SuccessStatus implements BaseCode {
 
+    // 일반 응답
     _OK(HttpStatus.OK, "COMMON200", "200 성공"),
-    _CREATED(HttpStatus.CREATED, "COMMON201", "201 성공");
+    _CREATED(HttpStatus.CREATED, "COMMON201", "성공적으로 생성되었습니다."),
+
+    // 유저 관련 응답
+    SIGN_IN_SUCCESS(HttpStatus.OK, "USER2011", "성공적으로 회원가입되었습니다."),
+    LOGIN_SUCCESS(HttpStatus.OK, "USER2001", "성공적으로 로그인되었습니다."),
+    LOGOUT_SUCCESS(HttpStatus.OK, "USER2002", "성공적으로 로그아웃되었습니다."),
+    USER_EDIT_SUCCESS(HttpStatus.OK, "USER2003", "유저 정보가 성공적으로 변경되었습니다."),
+    USER_SING_OUT_SUCCESS(HttpStatus.OK, "USER2004", "성공적으로 탈퇴되었습니다."),
+
+    // 관리자 페이지 관련 응답
+    ADMIN_LOGIN_SUCCESS(HttpStatus.OK, "ADMIN2011", "[관리자] 성공적으로 로그인되었습니다."),
+    ADMIN_LOGOUT_SUCCESS(HttpStatus.OK, "ADMIN2012", "[관리자] 성공적으로 로그아웃되었습니다."),
+    ADMIN_SIGN_OUT_SUCCESS(HttpStatus.OK, "ADMIN2013", "[관리자] 성공적으로 탈퇴되었습니다."),
+
+    // 플레이스 관련 응답
+    SAVE_PLACE_SUCCESS(HttpStatus.OK, "PLACE2001", "플레이스가 성공적으로 저장되었습니다."),
+    ADD_PLACE_SUCCESS(HttpStatus.OK, "PLACE2011", "[관리자] 플레이스가 성공적으로 등록되었습니다."),
+    EDIT_PLACE_SUCCESS(HttpStatus.OK, "PLACE2002", "[관리자] 플레이스가 성공적으로 수정되었습니다."),
+    DELETE_PLACE_SUCCESS(HttpStatus.OK, "PLACE2003", "[관리자] 플레이스가 성공적으로 삭제되었습니다."),
+
+    // 매거진 관련 응답
+    ADD_MAGAZINE_SUCCESS(HttpStatus.OK, "MAGAZINE2011", "[관리자] 매거진이 성공적으로 등록되었습니다."),
+    EDIT_MAGAZINE_SUCCESS(HttpStatus.OK, "MAGAZINE2001", "[관리자] 매거진이 성공적으로 수정되었습니다."),
+    DELETE_MAGAZINE_SUCCESS(HttpStatus.OK, "MAGAZINE2002", "[관리자] 플레이스가 성공적으로 삭제되었습니다."),
+
+    // 카테고리 관련 응답
+    ADD_CATEGORY_SUCCESS(HttpStatus.OK, "CATEGORY2010", "[관리자] 카테고리가 성공적으로 등록되었습니다."),
+    EDIT_CATEGORY_SUCCESS(HttpStatus.OK, "CATEGORY2001", "[관리자] 카테고리가 성공적으로 수정되었습니다."),
+    DELETE_CATEGORY_SUCCESS(HttpStatus.OK, "CATEGORY2002", "[관리자] 카테고리가 성공적으로 삭제되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
